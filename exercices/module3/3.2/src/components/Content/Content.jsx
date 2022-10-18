bfe80fdf8741e71913aa7deb7eb535f52bcc415a
dotched <danieldotche@outlook.be>
2022-10-18 15:10:57 +0200

    module3 : loading

---
+++ exercices/module3/3.2/src/components/Content/Content.jsx
@@ -0,0 +1,11 @@
+import Part from "components/Part/Part";
+
+const Content = (props)=>{
+    return(
+      <div>
+        {props.parts.map(part=><Part key={part.id} part={part.name} exercises={part.exercises}></Part>)}
+      </div>
+    )
+  }
+
+  export default Content;
